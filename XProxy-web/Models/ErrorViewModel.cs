52415389676f60b191ee0e7c8631c90bc1129585
dasp <victor@skalskiy.net>
2022-07-03 11:28:24 +0300

    first commit

---
+++ XProxy-web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace XProxy_web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
