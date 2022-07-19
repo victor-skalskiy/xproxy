73b1735464e054d77d188457c5678626c19da365
dasp <victor@skalskiy.net>
2022-07-19 20:38:15 +0300

    1-refac-config-usage

---
+++ XProxy.Services/XProxyOptions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using XProxy.Interfaces;
+
+namespace XProxy.Services;
+
+public class XProxyOptions : IXProxyOptions
+{
+    private readonly bool _uplink;
+    public XProxyOptions(IConfiguration configuration)
+    {
+        _uplink = configuration.GetSection("AppSetting").GetSection("Uplink").Value == "True";
+    }
+
+    bool IXProxyOptions.UpLink => _uplink;
+}
