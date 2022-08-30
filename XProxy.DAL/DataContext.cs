87ca9335453e5dc2d12eba8d76aa097f4d2d261a
Skalskiy Victor <victor@skalskiy.net>
2022-08-30 17:03:38 +0300

    add-user-identity
    adding scaffold Identity files

--- XProxy.DAL/DataContext.cs
+++ XProxy.DAL/DataContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace XProxy.DAL;
 
-public class DataContext : IdentityDbContext
+public class DataContext : IdentityDbContext<IdentityUser>
 {
     public DbSet<AV100FilterEntity> AV100Filters { get; set; }
     public DbSet<AV100RegionEntity> AV100Regions { get; set; }
